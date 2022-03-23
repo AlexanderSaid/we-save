0c63ee16f48449fd4b6ac7ea7013d527263f2386
Said YEGINOGLU <saidyein@Saids-Air.home>
2022-03-23 15:26:01 +0100

    nav.test comented out

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,17 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
-import TEST_ID from "./Nav.testid";
+// import TEST_ID from "./Nav.testid";
 
 const Nav = () => {
   return (
     <ul>
-      <Link to="/" data-testid={TEST_ID.linkToHome}>
+      {/* <Link to="/" data-testid={TEST_ID.linkToHome}>
         <li>Home</li>
       </Link>
       <Link to="/user" data-testid={TEST_ID.linkToUsers}>
         <li>Users</li>
-      </Link>
+      </Link> */}
     </ul>
   );
 };
