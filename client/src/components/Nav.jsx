c23f51114e59db0129f334b5497664002c350e8b
Abdulrahmansadek <boody.sad@gmail.com>
2022-03-23 16:21:39 +0100

    tailwindcss created

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { Link } from "react-router-dom";
+//import { Link } from "react-router-dom";
 
-import TEST_ID from "./Nav.testid";
+//import TEST_ID from "./Nav.testid";
 
 const Nav = () => {
   return (
     <ul>
-      <Link to="/" data-testid={TEST_ID.linkToHome}>
+      {/* <Link to="/" data-testid={TEST_ID.linkToHome}>
         <li>
           <p className="text-xl leading-tight">Home</p>
         </li>
@@ -16,16 +16,7 @@ const Nav = () => {
           {" "}
           <p className="text-xl leading-tight">User</p>
         </li>
-      </Link>
-      <div className="flex-col justify-center">
-        <div className=" bg-primary box font-title ">primary</div>
-        <div className=" bg-accent box font-title1">accent</div>
-        <div className=" bg-darkBg box">dark bg</div>
-        <div className=" bg-lightBg box">light bg</div>
-        <div className=" bg-darkFont box">dark font</div>
-        <div className=" bg-lightFont box">light font</div>
-        <div className=" bg-error box">error</div>
-      </div>
+      </Link> */}
     </ul>
   );
 };
