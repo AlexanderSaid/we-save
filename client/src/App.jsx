97e6319c1049c2742eb694293dab7b2b4748a161
AlexanderSaid <alexander.t.said@gmail.com>
2022-03-23 10:14:43 +0100

    remove unrelated files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
     <>
       <Nav />
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </>
   );
