60a5c80ad08e90df8fcc89b4f6783b7716d8f713
Said YEGINOGLU <saidyein@Saids-Air.home>
2022-03-23 15:27:54 +0100

    nav.test comented out, layout final draft

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Nav from "./components/Nav";
 import Footer from "./components/Footer";
 import Home from "./pages/Home/Home";
 import NavBar from "./components/NavBar";
@@ -8,7 +7,6 @@ import NavBar from "./components/NavBar";
 const App = () => {
   return (
     <>
-      <Nav />
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
