2e8eea2fa796e6826c91a58fc39af63491e1ab25
Bachar Daowd <bachar.daowd@gmail.com>
2022-03-24 14:15:50 +0100

    shos route

--- client/src/pages/Baskets/Baskets.jsx
+++ client/src/pages/Baskets/Baskets.jsx
@@ -1,13 +1,18 @@
-import React from "react";
-import SetLocationSection from "../../components/SetLocationSection/SetLocationSection";
+import { useEffect, useState } from 'react'
+import useFetch from '../../hooks/useFetch'
+
+import SetLocationSection from '../../components/SetLocationSection/SetLocationSection'
 
 const Baskets = () => {
+  const [shops, setShops] = useState([])
+  const { isLoading, error, performFetch, cancelFetch } = useFetch('/api/shops')
+  
   return (
     <div>
       <SetLocationSection />
       <h1>Baskets Page</h1>
     </div>
-  );
-};
+  )
+}
 
-export default Baskets;
+export default Baskets
