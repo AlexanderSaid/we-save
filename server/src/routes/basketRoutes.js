52d04bd8a0c5d967b7c17b85994559b132d9e0c0
Bachar Daowd <bachar.daowd@gmail.com>
2022-03-25 21:41:41 +0100

    fetch baskets by category route

---
+++ server/src/routes/basketRoutes.js
@@ -0,0 +1,13 @@
+import express from "express";
+const router = express.Router();
+import {
+  getAllBasket,
+  getByCategoryBasket,
+} from "../controllers/basketController.js";
+
+router.route("/").get(getAllBasket);
+router.route("/:category").get(getByCategoryBasket);
+
+export default router;
+
+
