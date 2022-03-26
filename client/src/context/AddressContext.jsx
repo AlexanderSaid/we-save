3148057fae9cac8d621521b2d38f25a3778fc20d
George Roumieh <george95.2621@gmail.com>
2022-03-26 18:07:58 +0100

    notification for post code

--- client/src/context/AddressContext.jsx
+++ client/src/context/AddressContext.jsx
@@ -9,6 +9,9 @@ export const AddressProvider = ({ children }) => {
     longitude: null,
   });
   const [postcode, setPostcode] = useState(null);
+  const [notification, setNotification] = useState("");
+  const [currentLocation, setCurrentLocation] = useState(false);
+  let addressFormatted = [];
 
   const handleLocation = (e) => {
     setLocation(e.target.value);
@@ -30,7 +33,7 @@ export const AddressProvider = ({ children }) => {
 
   useEffect(() => {
     getPostCode();
-  }, [coordinates]);
+  }, [currentLocation]);
 
   async function getAddress() {
     if (!location) return;
@@ -38,12 +41,28 @@ export const AddressProvider = ({ children }) => {
       const response = await fetch(
         `https://api.geoapify.com/v1/geocode/autocomplete?text=${location}&apiKey=8df64a19e0e54e67ac4cd1f80cff96a0`
       );
+
       const address = await response.json();
 
       setCoordinates({
         latitude: address.features[0].properties.lat,
         longitude: address.features[0].properties.lon,
       });
+      address.features.forEach((item) => {
+        addressFormatted.push(item.properties.formatted);
+      });
+
+      if (location.length >= 6) {
+        if (address.features[0].properties.city != "Amsterdam") {
+          setNotification(
+            "We are now available in the city of Amsterdam We hope to expand our services to your city soon."
+          );
+        } else {
+          setNotification("");
+        }
+      } else {
+        setNotification("");
+      }
     } catch (error) {
       // eslint-disable-next-line no-console
       console.error();
@@ -58,6 +77,7 @@ export const AddressProvider = ({ children }) => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(getCoordinates);
       setLocation("");
+      setCurrentLocation(true);
     } else {
       alert("Geolocation is not supported by the browser");
     }
@@ -76,6 +96,8 @@ export const AddressProvider = ({ children }) => {
     getLocation,
     postcode,
     location,
+    addressFormatted,
+    notification,
   };
 
   return (
