786e11e9e14f413833e808f1a256b7a24aa1e94d
Bachar Daowd <bachar.daowd@gmail.com>
2022-03-31 15:43:22 +0200

    Fetching a single shop

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,5 @@
 import express from "express";
 import cors from "cors";
-import errorHandler from "./middleware/errorMiddleware.js";
 
 import shopRouter from "./routes/shopsRoutes.js";
 import basketRouter from "./routes/basketRoutes.js";
@@ -24,6 +23,5 @@ app.use("/api/shops", shopRouter);
 app.use("/api/baskets", basketRouter);
 app.use("/api/users", userRouter);
 
-app.use(errorHandler);
 
 export default app;
