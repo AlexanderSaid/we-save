37e80bdb47e0f332d678ca4a35c50c09fd6efc08
Bachar Daowd <bachar.daowd@gmail.com>
2022-04-05 08:00:09 +0200

    The login and shopCreating and shopDelete Backend

--- server/src/controllers/shopsControllers.js
+++ server/src/controllers/shopsControllers.js
@@ -1,15 +1,16 @@
 import Shop from "../models/Shop.js";
 // import Basket from "../models/Basket.js";
 import asyncHandler from "express-async-handler";
+import User from "../models/User.js";
+import axios from "axios";
 
 //@des Get a single Shop
-//@route GET /api/shops/:id
+//@route GET /api/shops/:shopId
 //@access public
-
 const getSingleShop = asyncHandler(async (req, res) => {
-  const { id } = req.params;
+  const { shopId } = req.params;
   //get shop
-  const shop = await Shop.findById(id).populate("baskets");
+  const shop = await Shop.findById(shopId).populate("baskets");
   if (!shop) {
     res.status(401).json({ msg: "Shop not found" });
   }
@@ -28,4 +29,102 @@ const getAllShops = asyncHandler(async (req, res) => {
   res.status(201).json({ success: true, result: shops });
 });
 
-export { getAllShops, getSingleShop };
+//@des Create a Shop
+//@route POST /api/shops
+//@access Private
+const createShop = asyncHandler(async (req, res) => {
+  const {
+    name,
+    street,
+    house,
+    addition,
+    postcode,
+    city,
+    country,
+    phone,
+    email,
+    kvk,
+    iban,
+    image,
+    logo,
+    description,
+  } = req.body;
+  if (
+    !name ||
+    !street ||
+    !house ||
+    !postcode ||
+    !city ||
+    !country ||
+    !kvk ||
+    !iban ||
+    !image ||
+    !logo
+  ) {
+    res.status(400).json({ msg: "Please fill all the fields" });
+  }
+  const { data } = await axios.get(
+    `https://api.geoapify.com/v1/geocode/search?postcode=${postcode}&format=json&apiKey=0295f24387ed41c99bc8805b138ace7c`
+  );
+  if (data.results.length === 0) {
+    res.status(400).json({ msg: "Invalid Postcode" });
+  }
+  const address = {
+    street,
+    house,
+    addition: addition ? addition : "",
+    postcode,
+    city,
+    country,
+    lat: data.results[0].lat,
+    lon: data.results[0].lon,
+  };
+  const user = await User.findById(req.user.id);
+  if (!user) {
+    res.status(401).json({ msg: "User not found" });
+  }
+  const newShop = await Shop.create({
+    name,
+    address,
+    phone: phone ? phone : "",
+    email: email ? email : user.email,
+    kvk,
+    iban,
+    image,
+    logo,
+    description,
+    owner_id: req.user.id,
+  });
+  user.is_owner = true;
+  await user.save();
+  //onDelete the shop user.is_owner = false
+  res.status(201).json({ success: true, result: newShop });
+});
+
+//@des Delete a shop
+//@route DELETE /api/shops/:shopId
+//@access Private
+const deleteShop = asyncHandler(async (req, res) => {
+  const { shopId } = req.params;
+  //get user
+  const user = await User.findById(req.user.id);
+  if (!user) {
+    res.status(401).json("User not found");
+  }
+  const shop = await Shop.findById(shopId);
+
+  if (!shop) {
+    res.status(404).json("Shop not found");
+  }
+  if (shop.owner_id.toString() !== req.user.id) {
+    res.status(401).json("Not Authorized");
+  }
+  await shop.remove();
+  user.is_owner = false;
+  await user.save();
+  res
+    .status(200)
+    .json({ success: true, result: "Your Shop Was Deleted Successfully" });
+});
+
+export { getAllShops, getSingleShop, createShop, deleteShop };
--- server/src/routes/shopsRoutes.js
+++ server/src/routes/shopsRoutes.js
@@ -1,8 +1,14 @@
 import express from "express";
 const router = express.Router();
-import { getAllShops, getSingleShop } from "../controllers/shopsControllers.js";
+import {
+  getAllShops,
+  getSingleShop,
+  createShop,
+  deleteShop,
+} from "../controllers/shopsControllers.js";
+import protect from "../middleware/authMiddleware.js";
 
-router.route("/").get(getAllShops);
-router.route("/:id").get(getSingleShop);
+router.route("/").get(getAllShops).post(protect, createShop);
+router.route("/:shopId").get(getSingleShop).delete(protect, deleteShop);
 
 export default router;
