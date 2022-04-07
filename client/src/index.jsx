2a68904125d2702df1f656974982d71b18728596
George Roumieh <george95.2621@gmail.com>
2022-04-07 12:54:30 +0200

    shop regiseration conditions

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,18 +4,21 @@ import AppWrapper from "./AppWrapper";
 import App from "./App";
 import { Routes, Route } from "react-router-dom";
 import { SearchProvider } from "./context/SearchContext";
+import { SignInProvider } from "./context/SignInContext";
 
 import { UserProvider } from "./context/UserContext";
 
 ReactDOM.render(
   <AppWrapper>
-    <UserProvider>
-      <SearchProvider>
-        <Routes>
-          <Route path="/*" element={<App />} />
-        </Routes>
-      </SearchProvider>
-    </UserProvider>
+    <SignInProvider>
+      <UserProvider>
+        <SearchProvider>
+          <Routes>
+            <Route path="/*" element={<App />} />
+          </Routes>
+        </SearchProvider>
+      </UserProvider>
+    </SignInProvider>
   </AppWrapper>,
   document.getElementById("root")
 );
