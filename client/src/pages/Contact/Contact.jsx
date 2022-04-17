50d32589e2457550aad6a3f03c5000bff916d030
Bachar Daowd <bachar.daowd@gmail.com>
2022-04-17 12:19:02 +0200

    contactUs-motion

--- client/src/pages/Contact/Contact.jsx
+++ client/src/pages/Contact/Contact.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import GetInTouch from "./ContactComponents/GetInTouch";
 import EnterData from "./ContactComponents/EnterData";
+import { motion } from "framer-motion";
+import { pageAnimation } from "../../animation";
 
 const Contact = () => {
   return (
-    <section className="flex items-center justify-center bg-gradient-to-bl from-lightFont to-lightBg w-[95%]">
+    <motion.section
+      variants={pageAnimation}
+      className="flex items-center justify-center bg-gradient-to-bl from-lightFont to-lightBg w-[95%]"
+    >
       <div className="max-w-[500px] lg:flex lg:items-stretch lg:justify-center lg:max-w-[1024px]  my-12 shadow-2xl shadow-darkBg rounded-xl overflow-hidden">
         <GetInTouch />
         <EnterData />
       </div>
-    </section>
+    </motion.section>
   );
 };
 
