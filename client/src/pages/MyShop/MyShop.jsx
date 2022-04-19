701661537f9ca80e7e6ad11e1d943a8a04f14e0c
Bachar Daowd <bachar.daowd@gmail.com>
2022-04-19 11:52:23 +0200

    motion.shop

--- client/src/pages/MyShop/MyShop.jsx
+++ client/src/pages/MyShop/MyShop.jsx
@@ -5,6 +5,8 @@ import CreateBasketForm from "./Components/CreateBasketForm";
 import BasketSummary from "./Components/BasketSummary";
 import CoverShop from "./Components/CoverShop";
 import DeleteShopMessage from "./Components/DeleteShopMessage";
+import { motion } from "framer-motion";
+import { pageAnimation, fade } from "../../components/animation";
 
 //- Common classes
 function MyShop() {
@@ -50,40 +52,78 @@ function MyShop() {
   return (
     <>
       {deletePopup && <DeleteShopMessage setDeleteShop={setDeletePopup} />}
-      <section className="w-[100%] max-w-[1140px] p-4 mx-auto bg-lightBg ">
-        <CoverShop />
-        <h1 className="mb-4 text-3xl text-center text-accent">YOUR BASKETS</h1>
+      <motion.section
+        variants={pageAnimation}
+        initial="hidden"
+        animate="show"
+        className="w-[100%] max-w-[1140px] p-4 mx-auto bg-lightBg "
+      >
+        <motion.div variants={fade}>
+          <CoverShop />
+        </motion.div>
+        <motion.h1
+          variants={fade}
+          className="mb-4 text-3xl text-center text-accent"
+        >
+          YOUR BASKETS
+        </motion.h1>
 
         {baskets.length === 0 ? (
-          <p className="mb-8 text-center text-black-400">
+          <motion.p variants={fade} className="mb-8 text-center text-black-400">
             You dont have any baskets right now!
-          </p>
+          </motion.p>
         ) : (
           <div className=" justify-center grid grid-flow-col overflow-x-auto auto-cols-max  md:auto-cols-min rounded-md shadow-lg mb-[40px]">
             {baskets.map((basket, index) => {
               return (
-                <BasketSummary
-                  getBasket={getBasket}
-                  basket={basket}
+                <motion.div
                   key={index}
-                />
+                  variants={{
+                    hidden: {
+                      opacity: 0,
+                      x: -50,
+                      y: -50,
+                    },
+                    show: {
+                      opacity: 1,
+                      x: 0,
+                      y: 0,
+                      transition: {
+                        duration: 0.5,
+                        delay: index * 0.2,
+                        ease: "easeOut",
+                      },
+                    },
+                  }}
+                  initial="hidden"
+                  animate="show"
+                >
+                  <BasketSummary
+                    getBasket={getBasket}
+                    basket={basket}
+                    key={index}
+                  />
+                </motion.div>
               );
             })}
           </div>
         )}
-        <h1 className="text-3xl text-center text-accent">
+        <motion.h1 variants={fade} className="text-3xl text-center text-accent">
           {basket ? "EDIT YOUR BASKET" : "CREATE A BASKET"}
-        </h1>
+        </motion.h1>
         <div className=" w-[100%] max-w-[1140px] p-4 mx-auto">
-          <CreateBasketForm basket={basket} setBasket={setBasket} />
+          <motion.div variants={fade}>
+            <CreateBasketForm basket={basket} setBasket={setBasket} />
+          </motion.div>
         </div>
-        <button
+        <motion.button
+          variants={fade}
           onClick={() => setDeletePopup(true)}
           className="delete-btn submit-btn"
         >
           Delete Shop
-        </button>
-      </section>
+        </motion.button>
+      </motion.section>
     </>
   );
 }
